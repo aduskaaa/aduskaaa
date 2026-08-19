2733ed14557d638d10551980321621d7a34b78ea
aduskaaa <aduskaaaaaaaaa@gmail.com>
2026-08-19 19:54:38 +0200

    Update site-data.js

--- assets/site-data.js
+++ assets/site-data.js
@@ -222,7 +222,7 @@ window.SITE = {
       { name: "laur1sxd", amount: "40,42 EUR" },
       { name: "Mr JP", amount: "16,12 EUR" },
       { name: "Artie", amount: "8,48 EUR" },
-      { name: "weskergood1234", amount: "6,24 EUR"},
+      { name: "weskergood1234", amount: "8,24 EUR"},
       { name: "Maxi", amount: "4,50 EUR" },
       { name: "Delby", amount: "3,51 EUR" },
       { name: "Xiguawangzi3399", amount: "3,48 EUR" },
