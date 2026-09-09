5f5f97c692a680e3c11a3698767be800b0833d1e
aduskaaa <aduskaaaaaaaaa@gmail.com>
2026-09-09 14:50:22 +0200

    Update site-data.js

--- assets/site-data.js
+++ assets/site-data.js
@@ -19,7 +19,7 @@ window.SITE = {
   /* ---------- Support Goal ---------- */
   supportGoal: {
     target: 25,
-    current: 0,
+    current: 10,
     currency: "EUR"
   },
 
@@ -244,8 +244,9 @@ window.SITE = {
     donations: [
       { name: "laur1sxd", amount: "40,42 EUR", amount_ru: "4 042,00 ₽" },
       { name: "Mr JP", amount: "16,12 EUR", amount_ru: "1 612,00 ₽" },
-      { name: "Artie", amount: "8,48 EUR", amount_ru: "848,00 ₽" },
       { name: "weskergood1234", amount: "10,24 EUR", amount_ru: "1 024,00 ₽"},
+      { name: "Xes Xand", amount: "10,00 EUR", amount_ru: "1 000,00 ₽"},
+      { name: "Artie", amount: "8,48 EUR", amount_ru: "848,00 ₽" },
       { name: "Maxi", amount: "8,00 EUR", amount_ru: "800,00 ₽" },
       { name: "Delby", amount: "3,51 EUR", amount_ru: "351,00 ₽" },
       { name: "Xiguawangzi3399", amount: "3,48 EUR", amount_ru: "348,00 ₽" },
