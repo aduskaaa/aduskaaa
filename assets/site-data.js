9432b0a384c2b1ffd2c5f6123e62c842c7e3ee4a
aduskaaa <aduskaaaaaaaaa@gmail.com>
2026-09-19 09:07:23 +0200

    Update site-data.js

--- assets/site-data.js
+++ assets/site-data.js
@@ -19,7 +19,7 @@ window.SITE = {
   /* ---------- Support Goal ---------- */
   supportGoal: {
     target: 25,
-    current: 14,
+    current: 27.6,
     currency: "EUR"
   },
 
@@ -243,8 +243,8 @@ window.SITE = {
   supporters: {
     donations: [
       { name: "laur1sxd", amount: "40,42 EUR", amount_ru: "~4 042,00 ₽" },
+      { name: "Modsfire Ltd", amount: "30,47 EUR", amount_ru: "~3 047,00 ₽" },
       { name: "Mr JP", amount: "18,12 EUR", amount_ru: "~1 812,00 ₽" },
-      { name: "Modsfire Ltd", amount: "17,41 EUR", amount_ru: "~1 741,00 ₽" },
       { name: "weskergood1234", amount: "12,24 EUR", amount_ru: "~1 224,00 ₽"},
       { name: "Xes Xand", amount: "10,00 EUR", amount_ru: "~1 000,00 ₽"},
       { name: "Artie", amount: "8,48 EUR", amount_ru: "~848,00 ₽" },
